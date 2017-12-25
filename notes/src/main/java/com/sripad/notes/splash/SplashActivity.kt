d6aa9d5e586573a0e2cd6c6438db9a24e46b0b7d
Sripadmanaban Subramanian <sripadmanaban.subramanian@bottlerocketstudios.com>
2017-12-25 11:36:24 -0600

    Navigating from Splash to Home after 2 seconds.

--- notes/src/main/java/com/sripad/notes/splash/SplashActivity.kt
+++ notes/src/main/java/com/sripad/notes/splash/SplashActivity.kt
@@ -1,15 +1,23 @@
 package com.sripad.notes.splash
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import android.view.Window
 import com.sripad.notes.R
+import com.sripad.notes.home.HomeActivity
+import io.reactivex.Observable
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import java.util.concurrent.TimeUnit
 
 /**
  * Activity that acts as the splash screen.
  */
 class SplashActivity : AppCompatActivity() {
+
+    private var timerDisposable: Disposable? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,5 +26,24 @@ class SplashActivity : AppCompatActivity() {
         // Hide both the navigation bar
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
         setContentView(R.layout.activity_splash)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        timerDisposable = Observable.interval(SPLASH_TIME_INTERVAL_SECONDS, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe {
+                    val homeActivityIntent = Intent(this, HomeActivity::class.java)
+                    startActivity(homeActivityIntent)
+                }
+    }
+
+    override fun onStop() {
+        super.onStop()
+        timerDisposable?.dispose()
+    }
+
+    companion object {
+        private const val SPLASH_TIME_INTERVAL_SECONDS = 2L
     }
 }

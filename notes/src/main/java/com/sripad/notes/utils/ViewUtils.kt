e54530470ab3c25d8df22767379ca1acd54cb153
Sripadmanaban Subramanian <sripadmanaban.subramanian@bottlerocketstudios.com>
2017-12-31 11:49:59 -0600

    Added a helper method on text view to set text when it is not null or blank and make it gone otherwise.

--- notes/src/main/java/com/sripad/notes/utils/ViewUtils.kt
+++ notes/src/main/java/com/sripad/notes/utils/ViewUtils.kt
@@ -1,6 +1,7 @@
 package com.sripad.notes.utils
 
 import android.view.View
+import android.widget.TextView
 
 fun View?.makeVisible() {
     if (this != null) visibility = View.VISIBLE
@@ -8,4 +9,14 @@ fun View?.makeVisible() {
 
 fun View?.makeGone() {
     if (this != null) visibility = View.GONE
+}
+
+fun TextView?.setTextOrGone(value: String?) {
+    this ?: return
+    if (value.isNullOrBlank()) {
+        makeGone()
+    } else {
+        makeVisible()
+        text = value
+    }
 }

a867948ffff92e8ef157bda511d9f807d7741d1d
Sripadmanaban Subramanian <sripadmanaban.subramanian@bottlerocketstudios.com>
2017-12-31 11:50:49 -0600

    Added basic adapter to show a list of notes.

--- notes/src/main/java/com/sripad/notes/home/HomeActivity.kt
+++ notes/src/main/java/com/sripad/notes/home/HomeActivity.kt
@@ -1,20 +1,32 @@
 package com.sripad.notes.home
 
+import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModelProvider
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import com.jakewharton.rxbinding2.support.v7.widget.itemClicks
+import com.sripad.database.agent.NoteInfo
 import com.sripad.notes.R
 import com.sripad.notes.note.NewNoteActivity
+import com.sripad.notes.utils.makeGone
+import com.sripad.notes.utils.makeVisible
+import com.sripad.notes.utils.setTextOrGone
 import com.sripad.notes.viewmodel.getViewModel
 import dagger.android.AndroidInjection
 import io.reactivex.disposables.CompositeDisposable
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.recycler_note_item.view.*
+import timber.log.Timber
 import javax.inject.Inject
 
 class HomeActivity : AppCompatActivity() {
 
     private val disposables = CompositeDisposable()
+    private val notesAdapter = NotesAdapter()
 
     private lateinit var homeViewModel: HomeViewModel
 
@@ -26,6 +38,7 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
 
         homeViewModel = viewModelFactory.getViewModel(this)
+        homeViewModel.uiModel.observe(this, Observer { model -> model?.let { onUiModelUpdated(it) } })
 
         toolbar.inflateMenu(R.menu.menu_home)
         val itemClickDisposables = toolbar.itemClicks()
@@ -38,10 +51,67 @@ class HomeActivity : AppCompatActivity() {
                 .subscribe()
 
         disposables.addAll(itemClickDisposables)
+
+        notes_list.adapter = notesAdapter
     }
 
     override fun onDestroy() {
         disposables.clear()
         super.onDestroy()
+    }
+
+    private fun onUiModelUpdated(homeUiModel: HomeUiModel) {
+        Timber.d("[onUiModelUpdated] Ui Model Updated: $homeUiModel")
+        when (homeUiModel) {
+            is HomeUiModel.AllNotes -> {
+                notes_list.makeVisible()
+                notesAdapter.updateList(homeUiModel.notes)
+                loading_spinner.makeGone()
+                empty_list_text.makeGone()
+            }
+            is HomeUiModel.ShowLoadingSpinner -> {
+                loading_spinner.makeVisible()
+                notes_list.makeGone()
+                empty_list_text.makeGone()
+            }
+            is HomeUiModel.Empty -> {
+                empty_list_text.makeVisible()
+                loading_spinner.makeGone()
+                notes_list.makeGone()
+            }
+        }
+    }
+}
+
+private class NoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    fun bindNoteInfo(noteInfo: NoteInfo) {
+        itemView.title.setTextOrGone(noteInfo.title)
+        itemView.gist.setTextOrGone(noteInfo.note)
+        val modifiedOn = noteInfo.modifiedOn
+        val modifiedTime = "${modifiedOn.dayOfWeek} ${modifiedOn.dayOfMonth}"
+        itemView.modified_on.setTextOrGone(modifiedTime)
+    }
+}
+
+private class NotesAdapter : RecyclerView.Adapter<NoteViewHolder>() {
+
+    private val notes = mutableListOf<NoteInfo>()
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        holder.bindNoteInfo(notes[position])
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.recycler_note_item, parent, false)
+        return NoteViewHolder(view)
+    }
+
+    override fun getItemCount() = notes.size
+
+    fun updateList(notes: List<NoteInfo>) {
+        this.notes.clear()
+        this.notes.addAll(notes)
+        notifyDataSetChanged()
     }
 }
--- notes/src/main/java/com/sripad/notes/home/HomeCompanion.kt
+++ notes/src/main/java/com/sripad/notes/home/HomeCompanion.kt
@@ -2,31 +2,44 @@ package com.sripad.notes.home
 
 import android.arch.lifecycle.ViewModel
 import com.jakewharton.rxrelay2.BehaviorRelay
+import com.sripad.database.agent.DatabaseAgent
+import com.sripad.database.agent.NoteInfo
 import com.sripad.notes.viewmodel.ConsumerLiveData
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
 import javax.inject.Inject
 
 sealed class HomeUiModel {
-    object NewNote : HomeUiModel()
+    data class AllNotes(val notes: List<NoteInfo>) : HomeUiModel()
+    object ShowLoadingSpinner : HomeUiModel()
+    object Empty : HomeUiModel()
 }
 
-internal class HomeViewModel @Inject constructor() : ViewModel() {
+internal class HomeViewModel @Inject constructor(databaseAgent: DatabaseAgent) : ViewModel() {
 
     private val homeUiRelay = BehaviorRelay.create<HomeUiModel>()
     private val homeUiDisposable: Disposable
 
+    private val loadNotesDisposable: Disposable
+
     val uiModel = ConsumerLiveData<HomeUiModel>()
 
     init {
-        homeUiDisposable = homeUiRelay.observeOn(AndroidSchedulers.mainThread()).subscribe(uiModel)
-    }
-
-    fun addNewNote() {
-        homeUiRelay.accept(HomeUiModel.NewNote)
+        homeUiDisposable = homeUiRelay.distinctUntilChanged().observeOn(AndroidSchedulers.mainThread()).subscribe(uiModel)
+        loadNotesDisposable = databaseAgent.retrieveNotes()
+                .map {
+                    if (it.isEmpty()) {
+                        HomeUiModel.Empty
+                    } else {
+                        HomeUiModel.AllNotes(it)
+                    }
+                }
+                .startWith(HomeUiModel.ShowLoadingSpinner)
+                .subscribe { homeUiRelay.accept(it) }
     }
 
     override fun onCleared() {
+        loadNotesDisposable.dispose()
         homeUiDisposable.dispose()
     }
 }
